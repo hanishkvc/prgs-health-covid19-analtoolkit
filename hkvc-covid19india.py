Cov19In: moving average based fix missing values as a new function
initial structuring of the logic

hkvc-covid19india.py
@@ -42,6 +42,25 @@ def _fix_cumu(ndIn, iMissing):
     return ndIn
 
 
+def _fix_use_movavg(ndIn, iMissing):
+    nans = np.argwhere(np.isnan(data))
+    data[np.isnan(data)] = 0
+    for i in nans:
+        iS = i[0]-3
+        if (iS < 0):
+            iS = 0
+        iE = i[0]+3+1
+        if (iE > data.shape[0]):
+            iE = data.shape[0]
+        print("WARN:extract_data:fix_nan:at={}:using={}-{}".format(i, iS, iE))
+        print("\tIN:{}".format(data[iS:iE,i[1]]))
+        tWeights = np.ones(iE-iS)*1/6
+        data[i] = np.sum(data[iS:iE,i[1]]*tWeights)
+        print("\tNEW:{}".format(data[iS:iE,i[1]]))
+
+
+
+
 def get_data(ts, theUrl):
     tFile = "data/{}-covid19india_org-{}".format(ts, os.path.basename(theUrl))
     if os.path.exists(tFile) and (os.path.getsize(tFile)>128):
@@ -74,20 +93,6 @@ def extract_data_csv(tFile):
     # Skip the last column, which is invalid.
     data = data[:,0:-1]
     #data[12,1] = np.nan # For testing below logic to fix missing values is fine
-    nans = np.argwhere(np.isnan(data))
-    data[np.isnan(data)] = 0
-    for i in nans:
-        iS = i[0]-3
-        if (iS < 0):
-            iS = 0
-        iE = i[0]+3+1
-        if (iE > data.shape[0]):
-            iE = data.shape[0]
-        print("WARN:extract_data:fix_nan:at={}:using={}-{}".format(i, iS, iE))
-        print("\tIN:{}".format(data[iS:iE,i[1]]))
-        tWeights = np.ones(iE-iS)*1/6
-        data[i] = np.sum(data[iS:iE,i[1]]*tWeights)
-        print("\tNEW:{}".format(data[iS:iE,i[1]]))
     return data, legends
 
 