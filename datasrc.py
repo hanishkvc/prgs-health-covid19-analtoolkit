aa9420e7143b201a3575a872971958df09261d3c
hanishkvc <hanishkvc@gmail.com>
2020-04-23 22:53:41 +0530

    datasrc: use instance variables, Covid19India datasrc
    renamed _fix_url_filename into _fix_url_filenames, bcas it sets
    up url, fileName and dstFileName now.
    
    Updated download and _fix_url_filenames to use instance variables
    by default.
    
    download can take the url and dstFileName specified by the caller
    or else it picks these up from the instance variables of the class.
    
    As the logic used to fix up the url, web filename and dest filename
    is not known to the base class, so changed it to not accept any
    arguments, bcas the base class doesnt know what and all it requires.
    Instead it picks up the required info from its own class instance
    variables, which will be setup by the specific child class.
    
    Also implemented a child class for getting Covid19India website's
    covid data as a datasrc.

--- datasrc.py
+++ datasrc.py
@@ -8,41 +8,68 @@ import subprocess
 
 class DataSrc:
 
-    def download(self, theUrl, dstFileName):
+    def download(self, theUrl = None, dstFileName = None):
+        if theUrl == None:
+            theUrl = self.url
+        if dstFileName == None:
+            dstFileName = self.dstFileName
         print("INFO:DataSrc:{}:downloading...".format(theUrl))
         tCmd = [ "wget", theUrl, "--output-document={}"]
         tCmd[2] = tCmd[2].format(dstFileName)
         subprocess.call(tCmd)
 
-    def fix_url_filename(self, url, fileName):
+
+    def _fix_url_filenames(self):
         raise NotImplementedError("DataSrc: Download data from net...")
+
 
     def _set_fetch_date(self, day, month, year):
         curTime = time.gmtime()
-        if day = None:
+        if day == None:
             self.fd_day = curTime.tm_mday
         else:
             self.fd_day = day
-        if month = None:
+        if month == None:
             self.fd_month = curTime.tm_mon
         else:
             self.fd_month = month
-        if year = None:
+        if year == None:
             self.fd_year = curTime.tm_year
         else:
             self.fd_year = year
 
-    def fetch_data(self, url, fileName, day=None, month=None, year=None):
-        _set_fetch_date(day, month, year)
-        url, fileName = fix_url_filename(url, fileName)
-        dstFileName = "data/{}-{}".format(self.name, fileName)
-        if os.path.exists(dstFileName) and (os.path.getsize(dstFileName)>128):
-            print("INFO:{}:already downloaded".format(dstFileName))
-            return True
-        download(url, dstFileName)
 
-    
-        
+    def fetch_data(self, day=None, month=None, year=None):
+        _set_fetch_date(day, month, year)
+        _fix_url_filenames()
+        if os.path.exists(self.dstFileName) and (os.path.getsize(self.dstFileName)>128):
+            print("INFO:DataSrc:{}:already downloaded".format(self.dstFileName))
+            return True
+        download()
+
+
+
+class Cov19InDataSrc(DataSrc):
+
+    #urlFmt = "http://api.covid19india.org/states_daily_csv/confirmed.csv"
+    fileNameFmt = "confirmed.csv"
+    urlFmt = "http://api.covid19india.org/states_daily_csv/{}"
+    dstFileNameFmt = "data/{}-{}{}{}-{}"
+
+    def _init_(self):
+        self.name = "Cov19In"
+
+
+    def _fix_url_filenames(self):
+        self.fileName = fileNameFmt
+        self.url = urlFmt.format(self.fileName)
+        self.dstFileName = dstFileNameFmt.format(self.name, self.fd_year, self.fd_month, self.fd_day, self.fileName)
+
+
+
+if __name__ == "__main__":
+    theDataSrc = Cov19InDataSrc()
+    theDataSrc.fetch_data()
 
 
 
