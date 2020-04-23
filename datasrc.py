7f8774b3fa85b97f871439f50174fcc685b36d5c
hanishkvc <hanishkvc@gmail.com>
2020-04-23 22:21:40 +0530

    set_fetch_date, download, fix_url_filename
    Moved the set fetch date logic into its own function.
    
    Implemented url download logic using wget in download function.
    Its no longer a abstract function.
    
    Rather now fix_url_filename is the abstract function which
    requires to be implemented by the child classes. The idea of
    keeping download abstact previously was for this purpose only.
    So moved it into a more appropriately named function.
    
    The destination file is stored in a data folder.

--- datasrc.py
+++ datasrc.py
@@ -4,16 +4,20 @@
 #
 
 import time
+import subprocess
 
 class DataSrc:
 
-    def download(self):
+    def download(self, theUrl, dstFileName):
+        print("INFO:DataSrc:{}:downloading...".format(theUrl))
+        tCmd = [ "wget", theUrl, "--output-document={}"]
+        tCmd[2] = tCmd[2].format(dstFileName)
+        subprocess.call(tCmd)
+
+    def fix_url_filename(self, url, fileName):
         raise NotImplementedError("DataSrc: Download data from net...")
 
-    def fetch_data(self, url, fileName, day=None, month=None, year=None):
-        if os.path.exists(fileName) and (os.path.getsize(fileName)>128):
-            print("INFO:{}:already downloaded".format(tFile))
-            return True
+    def _set_fetch_date(self, day, month, year):
         curTime = time.gmtime()
         if day = None:
             self.fd_day = curTime.tm_mday
@@ -27,12 +31,18 @@ class DataSrc:
             self.fd_year = curTime.tm_year
         else:
             self.fd_year = year
-        download(url, fileName)
+
+    def fetch_data(self, url, fileName, day=None, month=None, year=None):
+        _set_fetch_date(day, month, year)
+        url, fileName = fix_url_filename(url, fileName)
+        dstFileName = "data/{}-{}".format(self.name, fileName)
+        if os.path.exists(dstFileName) and (os.path.getsize(dstFileName)>128):
+            print("INFO:{}:already downloaded".format(dstFileName))
+            return True
+        download(url, dstFileName)
 
     
         
-
-
 
 
 
