c984cee7b171597ea97a1e5a6d1a01d69dc2b9c8
hanishkvc <hanishkvc@gmail.com>
2020-04-30 13:03:40 +0530

    Initial restructuring of main prog into few funcs
    THis is a simple restructuring of the logic. THe calls havent
    been optimised yet. THere is duplication of things.

--- hkvc-covid19-analtoolkit.py
+++ hkvc-covid19-analtoolkit.py
@@ -4,48 +4,69 @@
 #
 
 import sys
-import datasrc as ds
+import datasrc as dsrc
 import analplot
 import matplotlib.pyplot as plt
 import numpy as np
 
 
+def fetch():
+    dsEU = dsrc.EUWorldDataSrc()
+    dsC19In = dsrc.Cov19InDataSrc()
+    for ds in [ dsC19In, dsEU ]:
+        ds.fetch_data()
+        ds.load_data()
+    return dsEU, dsC19In
 
-dsEU = ds.EUWorldDataSrc()
-dsC19In = ds.Cov19InDataSrc()
 
+def plot_simple():
+    fig, axes = plt.subplots(4,2)
+    iCur = 0
+    sGlobalMsg = ""
+    for ds in [ dsC19In, dsEU ]:
+        ap.set_raw(ds.data[:,2:], ds.data[:,0], ds.hdr[2:])
+        ap.plot(axes[0,iCur], "raw", numXTicks=4, xtickMultOf=7)
+        ap.calc_rel2mean()
+        ap.plot(axes[1,iCur], "raw.rel2mean")
+        ap.calc_rel2sum()
+        ap.plot(axes[2,iCur], "raw.rel2sum")
+        ap.calc_movavg()
+        selCols = ap.selcols_percentiles("raw.movavg")
+        ap.plot(axes[3,iCur], "raw.movavg", plotSelCols=selCols)
+        sGlobalMsg += " {}:DataDate:{}-{};".format(ds.name, np.min(ds.data[:,0]), np.max(ds.data[:,0]))
+        iCur += 1
+    save_fig(fig, sGlobalMsg)
+
+
+def plot_sel():
+    fig, axes = plt.subplots(3,2)
+    iCur = 0
+    sGlobalMsg = ""
+    for ds in [ dsC19In, dsEU ]:
+        ap.set_raw(ds.data[:,2:], ds.data[:,0], ds.hdr[2:])
+        ap.plot(axes[0,iCur], "raw", numXTicks=4, xtickMultOf=7)
+        ap.calc_movavg()
+        selCols = ap.selcols_percentiles("raw.movavg", selPers=[0,20])
+        ap.plot(axes[1,iCur], "raw.movavg", plotSelCols=selCols, plotLegend=True)
+        selCols = ap.selcols_percentiles("raw.movavg", selPers=[80,100])
+        ap.plot(axes[2,iCur], "raw.movavg", plotSelCols=selCols, plotLegend=True)
+        sGlobalMsg += " {}:DataDate:{}-{};".format(ds.name, np.min(ds.data[:,0]), np.max(ds.data[:,0]))
+        iCur += 1
+    save_fig(fig, sGlobalMsg)
+
+
+def save_fig(fig, sMsg):
+    sMsg += " hkvc"
+    fig.text(0.01, 0.002, sMsg)
+    fig.set_tight_layout(True)
+    tFName = sMsg.replace(";","_N_").replace(" ","_")
+    fig.savefig("/tmp/{}.svg".format(tFName))
+    plt.show()
+
+
+dsEU, dsC19In = fetch()
 ap = analplot.AnalPlot()
-
-fig, axes = plt.subplots(4,2)
-iCur = 0
-sGlobalMsg = ""
-for ds in [ dsC19In, dsEU ]:
-    ds.fetch_data()
-    ds.load_data()
-    ap.set_raw(ds.data[:,2:], ds.data[:,0], ds.hdr[2:])
-    ap.plot(axes[0,iCur], "raw", numXTicks=4, xtickMultOf=7)
-    """
-    ap.calc_rel2mean()
-    ap.plot(axes[1,iCur], "raw.rel2mean")
-    ap.calc_rel2sum()
-    ap.plot(axes[2,iCur], "raw.rel2sum")
-    """
-    ap.calc_movavg()
-    selCols = ap.selcols_percentiles("raw.movavg")
-    ap.plot(axes[1,iCur], "raw.movavg", plotSelCols=selCols)
-    selCols = ap.selcols_percentiles("raw.movavg", selPers=[0,20])
-    ap.plot(axes[2,iCur], "raw.movavg", plotSelCols=selCols, plotLegend=True)
-    selCols = ap.selcols_percentiles("raw.movavg", selPers=[80,100])
-    ap.plot(axes[3,iCur], "raw.movavg", plotSelCols=selCols, plotLegend=True)
-    sGlobalMsg += " {}:DataDate:{}-{};".format(ds.name, np.min(ds.data[:,0]), np.max(ds.data[:,0]))
-    iCur += 1
-
-sGlobalMsg += " hkvc"
-fig.text(0.01, 0.002, sGlobalMsg)
-fig.set_tight_layout(True)
-tFName = sGlobalMsg.replace(";","_N_").replace(" ","_")
-fig.savefig("/tmp/{}.svg".format(tFName))
-plt.show()
-
+plot_simple()
+plot_sel()
 
 # vim: set softtabstop=4 expandtab shiftwidth=4: #
