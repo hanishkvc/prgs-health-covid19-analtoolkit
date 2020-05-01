b2cd47f1d1a3e1f8a7e9f896f4bec0ef1ceb9b4c
hanishkvc <hanishkvc@gmail.com>
2020-05-01 14:15:00 +0530

    A Better Diff based plot
    Plot MovAvgTimes3 DiffOfCases/Day data of TopN regions selected
    from MovAvgTimes2 DiffOfCases/Day data.
    
    Also inset DiffOfCases/Day data on top.

--- hkvc-covid19-analtoolkit.py
+++ hkvc-covid19-analtoolkit.py
@@ -40,7 +40,7 @@ def plot_simple():
 
 
 def plot_sel():
-    fig, axes = ap.subplots(plt,5,2)
+    fig, axes = ap.subplots(plt,4,2)
     iCur = 0
     sGlobalMsg = ""
     for ds in [ dsC19In, dsEU ]:
@@ -62,14 +62,12 @@ def plot_sel():
         ap.boxplot(axes[2,iCur], "raw", plotSelCols=selCols, bInsetBoxPlot=True, title="%s-Cases/Day-MovAvgTop12"%(ds.name))
         # Diff of Raw data
         ap.calc_diff()
-        ap.calc_movavg(dataSel="raw.diff")
-        ap.calc_movavg(dataSel="raw.diff.movavg")
-        selCols, selPers = ap.selcols_percentiles("raw.diff.movavg.movavg", topN=8)
-        ap.plot(axes[3,iCur], "raw.diff.movavg.movavg", plotSelCols=selCols, plotLegend=True, title="%s-Cases/Day_DiffMovAvgT2-DiffMovAvgT2Top8"%(ds.name))
-        inset = axes[3,iCur].inset_axes([0.13,0.55,0.64,0.4])
-        ap.plot(inset, "raw.diff", plotSelCols=selCols, bTranslucent=True, title="%s-Cases/Day_Diff-DiffMovAvg^2Top8"%(ds.name))
+        ap.calc_movavg_ex(dataSel="raw.diff", times=2)
         ap.calc_movavg_ex(dataSel="raw.diff", times=3)
-        ap.plot(axes[4,iCur], "raw.diff.movavgT3", plotSelCols=selCols, plotLegend=True, title="%s-Cases/Day_DiffMovAvgT3-DiffMovAvgT2Top8"%(ds.name))
+        selCols, selPers = ap.selcols_percentiles("raw.diff.movavgT2", topN=8)
+        ap.plot(axes[3,iCur], "raw.diff.movavgT3", plotSelCols=selCols, plotLegend=True, title="%s-DiffOfCases/Day_MovAvgT3-DiffMovAvgT2Top8"%(ds.name))
+        inset = axes[3,iCur].inset_axes([0.13,0.55,0.64,0.4])
+        ap.plot(inset, "raw.diff", plotSelCols=selCols, bTranslucent=True, title="%s-DiffOfCases/Day-DiffMovAvgT2Top8"%(ds.name))
         sGlobalMsg += " {}:DataDate:{}-{};".format(ds.name, np.min(ds.data[:,0]), np.max(ds.data[:,0]))
         iCur += 1
     save_fig(fig, sGlobalMsg)
