d29e874960077858ec47f50d2a33e7d5fe010221
hanishkvc <hanishkvc@gmail.com>
2020-05-02 16:19:39 +0530

    Explicitly set the base dataKey to cases/day
    instead of the default "raw"

--- hkvc-covid19-analtoolkit.py
+++ hkvc-covid19-analtoolkit.py
@@ -26,21 +26,21 @@ def plot_simple(allDS):
     sGlobalMsg = ""
     for ds in allDS:
         ap.new_dataset()
-        ap.set_raw(ds.data[:,2:], ds.data[:,0], ds.hdr[2:])
-        ap.plot(axes[0,iCur], "raw", numXTicks=4, xtickMultOf=7, title="%s-Cases/Day"%(ds.name))
+        ap.set_raw(ds.data[:,2:], ds.data[:,0], ds.hdr[2:], dataKey="cases/day")
+        ap.plot(axes[0,iCur], "cases/day", numXTicks=4, xtickMultOf=7, title="%s-Cases/Day"%(ds.name))
         ap.calc_rel2mean()
-        ap.plot(axes[1,iCur], "raw.rel2mean", title="%s-Cases/Day_Rel2Mean"%(ds.name))
+        ap.plot(axes[1,iCur], "cases/day.rel2mean", title="%s-Cases/Day_Rel2Mean"%(ds.name))
         ap.calc_rel2sum()
-        ap.plot(axes[2,iCur], "raw.rel2sum", title="%s-Cases/Day_Rel2Sum"%(ds.name))
+        ap.plot(axes[2,iCur], "cases/day.rel2sum", title="%s-Cases/Day_Rel2Sum"%(ds.name))
         ap.calc_movavg()
-        selCols, selPers = ap.selcols_percentiles("raw.movavg")
-        ap.plot(axes[3,iCur], "raw.movavg", plotSelCols=selCols, title="%s-Cases/Day_MovAvg"%(ds.name))
+        selCols, selPers = ap.selcols_percentiles("cases/day.movavg")
+        ap.plot(axes[3,iCur], "cases/day.movavg", plotSelCols=selCols, title="%s-Cases/Day_MovAvg"%(ds.name))
         sGlobalMsg += "{}-Data-{}_{}--".format(ds.name, np.min(ds.data[:,0]), np.max(ds.data[:,0]))
         iCur += 1
     save_fig(fig, sGlobalMsg)
 
 
-def plot_diffdata(ds, ap, axes, iARow, iACol, dataKey="raw", sBaseDataMsg="Cases/Day"):
+def plot_diffdata(ds, ap, axes, iARow, iACol, dataKey="cases/day", sBaseDataMsg="Cases/Day"):
     ap.calc_diff(dataKey)
     ap.calc_movavg_ex(dataKey="%s.diff"%(dataKey), times=2)
     ap.calc_movavg_ex(dataKey="%s.diff"%(dataKey), times=3)
@@ -68,25 +68,25 @@ def plot_sel(allDS):
         ap.new_dataset()
         dprint("DBUG:Main:plot_sel:hdr-type:%s" %(type(ds.hdr[-2])))
         # The Raw data
-        ap.set_raw(ds.data[:,2:], ds.data[:,0], ds.hdr[2:])
-        ap.plot(axes[0,iCur], "raw", numXTicks=4, xtickMultOf=7, title="%s-Cases/Day-All"%(ds.name))
+        ap.set_raw(ds.data[:,2:], ds.data[:,0], ds.hdr[2:], dataKey="cases/day")
+        ap.plot(axes[0,iCur], "cases/day", numXTicks=4, xtickMultOf=7, title="%s-Cases/Day-All"%(ds.name))
         # The moving avg
         ap.calc_movavg()
         """
-        selCols, selPers = ap.selcols_percentiles("raw.movavg", selPers=[0,15])
-        ap.plot(axes[1,iCur], "raw.movavg", plotSelCols=selCols, plotLegend=True)
+        selCols, selPers = ap.selcols_percentiles("cases/day.movavg", selPers=[0,15])
+        ap.plot(axes[1,iCur], "cases/day.movavg", plotSelCols=selCols, plotLegend=True)
         """
-        selCols, selPers = ap.selcols_percentiles("raw.movavg", topN=12)
+        selCols, selPers = ap.selcols_percentiles("cases/day.movavg", topN=12)
         yscale = "log"
         yscale = None
-        ap.plot(axes[1,iCur], "raw.movavg", plotSelCols=selCols, plotLegend=True, yscale=yscale, title="%s-Cases/Day_MovAvg-MovAvgTop12"%(ds.name))
+        ap.plot(axes[1,iCur], "cases/day.movavg", plotSelCols=selCols, plotLegend=True, yscale=yscale, title="%s-Cases/Day_MovAvg-MovAvgTop12"%(ds.name))
         # Boxplot Raw data
-        selCols, selPers = ap.selcols_percentiles("raw.movavg", topN=15)
-        ap.boxplot(axes[2,iCur], "raw", plotSelCols=selCols, bInsetBoxPlot=True, title="%s-Cases/Day-MovAvgTop15"%(ds.name))
+        selCols, selPers = ap.selcols_percentiles("cases/day.movavg", topN=15)
+        ap.boxplot(axes[2,iCur], "cases/day", plotSelCols=selCols, bInsetBoxPlot=True, title="%s-Cases/Day-MovAvgTop15"%(ds.name))
         # Diff of Raw data
         plot_diffdata(ds, ap, axes, 3, iCur)
         #ap.calc_rel2sum()
-        #plot_diffdata(ds, ap, axes, 4, iCur, dataKey="raw.rel2sum", sBaseDataMsg="Rel2SumOfCases/Day")
+        #plot_diffdata(ds, ap, axes, 4, iCur, dataKey="cases/day.rel2sum", sBaseDataMsg="Rel2SumOfCases/Day")
         sGlobalMsg += "{}-Data-{}_{}--".format(ds.name, np.min(ds.data[:,0]), np.max(ds.data[:,0]))
         iCur += 1
     save_fig(fig, sGlobalMsg)
